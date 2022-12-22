3bddd396eb15f26d5fc04e31d1d029cdeb9ee965
soapgu <ghost_gu@163.com>
2022-12-22 18:00:53 +0800

    feat: 🎸 导航跳转A到B

---
+++ app/src/main/java/com/demo/navigationpopup/AFragment.java
@@ -0,0 +1,65 @@
+package com.demo.navigationpopup;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AFragment extends Fragment {
+
+    private static int aIndex = 0;
+    private int index;
+
+    public AFragment() {
+        // Required empty public constructor
+        aIndex ++;
+        index = aIndex;
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     * @return A new instance of fragment AFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AFragment newInstance() {
+        AFragment fragment = new AFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_a, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView textView = view.findViewById(R.id.msg);
+        textView.setText( String.format("This is %d AFragment!",index ) );
+        view.findViewById(R.id.btn_to_b).setOnClickListener( v -> {
+            NavDirections action = AFragmentDirections.actionAFragmentToBFragment();
+            Navigation.findNavController(view).navigate(action);
+        });
+    }
+}
---
+++ app/src/main/java/com/demo/navigationpopup/BFragment.java
@@ -0,0 +1,57 @@
+package com.demo.navigationpopup;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BFragment extends Fragment {
+
+    private static int bIndex = 0;
+    private int index;
+
+    public BFragment() {
+        bIndex ++;
+        this.index = bIndex;
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment BFragment.
+     */
+    public static BFragment newInstance() {
+        return new BFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_b, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView textView = view.findViewById(R.id.msg);
+        textView.setText( String.format("This is %d BFragment!",index ) );
+    }
+}
