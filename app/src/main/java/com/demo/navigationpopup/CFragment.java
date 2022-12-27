d29ee8da726b21511e8de48577d091ff18457195
soapgu <ghost_gu@163.com>
2022-12-27 12:18:49 +0800

    feat: 🎸 增加跨页面的回退操作

--- app/src/main/java/com/demo/navigationpopup/CFragment.java
+++ app/src/main/java/com/demo/navigationpopup/CFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
@@ -15,27 +14,27 @@ import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link BFragment#newInstance} factory method to
+ * Use the {@link CFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class BFragment extends Fragment {
+public class CFragment extends Fragment {
 
-    private static int bIndex = 0;
+    private static int cIndex = 0;
     private final int index;
 
-    public BFragment() {
-        bIndex ++;
-        this.index = bIndex;
+    public CFragment() {
+        cIndex ++;
+        index = cIndex;
     }
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @return A new instance of fragment BFragment.
+     * @return A new instance of fragment CFragment.
      */
-    public static BFragment newInstance() {
-        return new BFragment();
+    public static CFragment newInstance(String param1, String param2) {
+        return new CFragment();
     }
 
     @Override
@@ -47,25 +46,16 @@ public class BFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_b, container, false);
+        return inflater.inflate(R.layout.fragment_c, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         TextView textView = view.findViewById(R.id.msg);
-        textView.setText( String.format("This is %d BFragment!",index ) );
+        textView.setText( String.format("This is %d CFragment!",index ) );
         view.findViewById(R.id.btn_back_a).setOnClickListener( v -> {
-            /*
-            NavDirections action = BFragmentDirections.actionBFragmentToAFragment();
-            Navigation.findNavController(view).navigate(action);
-             */
-            Navigation.findNavController(view).popBackStack();
-        } );
-
-        view.findViewById(R.id.btn_to_c).setOnClickListener( v-> {
-            NavDirections action = BFragmentDirections.actionBFragmentToCFragment();
-            Navigation.findNavController(view).navigate(action);
+            Navigation.findNavController(view).popBackStack(R.id.AFragment,false);
         } );
     }
 }
